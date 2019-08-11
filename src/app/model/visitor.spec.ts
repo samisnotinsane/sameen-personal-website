1e608243df5839fcd281bc0b2bc224d51bd35848
samisnotinsane <sameen11@hotmail.com>
2019-08-11 13:53:57 +0100

    Adding test for visitor

--- src/app/model/visitor.spec.ts
+++ src/app/model/visitor.spec.ts
@@ -2,6 +2,7 @@ import { Visitor } from './visitor';
 
 describe('Visitor', () => {
   it('should create an instance', () => {
-    expect(new Visitor()).toBeTruthy();
+    expect(new Visitor(4, 'Samantha Fox', '000-000-000', 'samantha.fox@abc.com', 'Hey'))
+      .toBeTruthy();
   });
 });
